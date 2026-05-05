a5bb29e073aed83f85238e95819ef1a5b652510a
Francis Olamilekan Ajayi <lekanfaa@gmail.com>
2026-05-05 13:04:13 +0100

    Delete customer_segment_comparison.sql

--- customer_segment_comparison.sql
+++
@@ -1,9 +0,0 @@
-select
-customer_segment,
-count(order_id) as total_orders,
-round(avg(quantity * unit_price), 2) as
-avg_order_value,
-sum(quantity * unit_price) as total_revenue
-from ng_sales
-group by customer_segment
-order by avg_order_value desc;
